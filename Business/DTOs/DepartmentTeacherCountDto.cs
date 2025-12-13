568f9872ca45c610c5ccb38c8698e57dfe8cca48
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-13 15:46:43 +0100

    Create StaffService and DTO for first app requirement

---
+++ Business/DTOs/DepartmentTeacherCountDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4.Business.DTOs
+{
+    public class DepartmentTeacherCountDto
+    {
+        public string DepartmentTitle { get; set; }
+        public int NumberOfTeachers { get; set; }
+    }
+}
---
+++ Business/Services/StaffService.cs
@@ -0,0 +1,31 @@
+﻿using Lab4.Business.DTOs;
+using Lab4.Data;
+using Lab4.Data.Interfaces;
+using Lab4.Data.Repositories;
+using Lab4.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Lab4.Business.Services
+{
+    internal class StaffService
+    {
+        private readonly IStaffRepository _staffRepository;
+        private readonly IDepartmentRepository _departmentRepository;
+
+        public StaffService(IStaffRepository staffRepository, IDepartmentRepository departmentRepository)
+        {
+            _staffRepository = staffRepository;
+            _departmentRepository = departmentRepository;
+        }
+
+        public List<DepartmentTeacherCountDto> AmountOfTeachersPerDepartment()
+        {
+            var allDepartments = _departmentRepository.GetAllDepartments();
+
+            return allDepartments.Select(d => new DepartmentTeacherCountDto {DepartmentTitle = d.Title, NumberOfTeachers = d.Staff.Count(s => s.StaffTypeId == 1)}).ToList();
+        }
+    }
+}
