2c18a43daca4dda01b73b069b343e6af5e8e8e55
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-13 17:01:34 +0100

    Add necessary class members and constructor

--- Data/Repositories/StudentRepository.cs
+++ Data/Repositories/StudentRepository.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Lab4.Data.Interfaces;
+using Lab4.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Lab4.Data.Repositories
 {
-    internal class StudentRepository
+    internal class StudentRepository : IStudentRepository
     {
+        private readonly Lab4DBContext _context;
+
+        public StudentRepository(Lab4DBContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Student> GetAllStudents()
+        {
+            return _context.Students;
+        }
     }
 }
