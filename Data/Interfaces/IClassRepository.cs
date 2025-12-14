d5288488ea93c0fad7fdeddca0696f14eb6cacb0
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-12-14 20:01:23 +0100

    First draft of class

---
+++ Data/Interfaces/IClassRepository.cs
@@ -0,0 +1,12 @@
+﻿using Lab4.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4.Data.Interfaces
+{
+    internal interface IClassRepository
+    {
+        IQueryable<Class> GetAllClasses();
+    }
+}
